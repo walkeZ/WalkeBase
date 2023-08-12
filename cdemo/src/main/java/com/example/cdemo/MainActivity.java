d372f14cf45ff8f780dec0d2a2d43b4cae4c02f0
walke <1126648815@qq.com>
2023-08-12 17:58:08 +0800

    新建一个项目-用于学习Android调用C语言

---
+++ cdemo/src/main/java/com/example/cdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.cdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author Walker - 2023/8/12 17:30 PM
+ * @email 1126648815@qq.ocm
+ * @desc : 新建一个使用C语言的Demo
+ * 参考：https://www.51c51.com/danpianji/xinxi/84/786987.html
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
