8fb78b3ddc71dd2ccc84b7f2b155d8c30ed14efe
walke <1126648815@qq.com>
2024-05-16 13:56:04 +0800

    Android mvvmDemo first

---
+++ mvvmdemo/src/main/java/com/walker/mvvmdemo/MvvmActivity.java
@@ -0,0 +1,14 @@
+package com.walker.mvvmdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MvvmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvvm);
+    }
+}
